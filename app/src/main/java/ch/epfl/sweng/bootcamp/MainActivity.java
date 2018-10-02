4ad6fc3079084b39958be1b116059cdf9cd9cb69
nazliuzuner <uzuner.nazlideniz@epfl.ch>
2018-10-02 23:25:18 +0200

    final part added, problems solved

--- app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -1,14 +1,32 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void sendMessage(View view) {
+        Intent intent;
+        intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.mainName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
